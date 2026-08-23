03ae74e5664fc949f541a67acd0b570ed5056dec
evgeniystalker <evgenky94@mail.ru>
2026-08-24 02:06:50 +0500

    Тестовый проект

---
+++ ValheimFTPSync.Test/FtpClientTests.cs
@@ -0,0 +1,77 @@
+﻿using Moq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using System;
+using System.IO;
+using ValheimFTPSync.Services;
+using System.Text;
+
+namespace ValheimFTPSync.Test
+{
+    public class FtpClientTests
+    {
+
+        Uri realFtpUri = new Uri("ftp://192.168.1.1:21/sda1/Valheim/");
+        NetworkCredential credentials = new NetworkCredential();
+        public FtpClientTests()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+        }
+        [Fact]
+        public async Task GetDateTimeStampAsync_RealNetwork_ShouldReturnValidTime()
+        {
+            var client = new FtpClient(realFtpUri, credentials);
+
+            // Имя реально существующего файла в папке Valheim на FTP для проверки
+            // (Замените "server.cfg", если там лежит другой файл, например "world.db")
+            string realFileName = "Player-prev.log";
+
+            // 2. Act: Выполняем реальный запрос в вашу сеть
+            DateTime actualTime = await client.GetDateTimeStampAsync(realFileName, CancellationToken.None);
+
+            // 3. Assert: Проверяем базовые свойства вернувшейся даты
+            // Мы не знаем точную дату, но можем проверить, что она успешно прочитана
+            Assert.NotEqual(DateTime.MinValue, actualTime);
+            Assert.Equal(DateTimeKind.Utc, actualTime.Kind);
+
+            // Выводим дату в консоль теста для ручного контроля
+            Console.WriteLine($"Успешно! Дата изменения файла на FTP (UTC): {actualTime}");
+        }
+
+        [Fact(Skip = "Real File")]
+        public async Task UploadFile_RealNetwork_CheckDate()
+        {
+            var client = new FtpClient(realFtpUri, credentials);
+            FileStream file = File.OpenRead("D:\\Users\\evgen\\Desktop\\Player-prev.log");
+            await client.UploadFileAsync("Player-prev.log", file);
+
+            DateTime dateTimeUploadFile = (await client.GetDateTimeStampAsync("Player-prev.log")).ToLocalTime();
+            var now = DateTime.Now;
+            Assert.True(dateTimeUploadFile.Hour == now.Hour && dateTimeUploadFile.Minute == now.Minute && dateTimeUploadFile.Date == now.Date);
+        }
+        [Fact]
+        public async Task ListDirectory_RealNetwork_CheckDate()
+        {
+            var client = new FtpClient(realFtpUri, credentials);
+            var list = await client.ListDirectoryAsync(null!, CancellationToken.None);
+            Assert.NotEmpty(list);
+        }
+
+        [Fact]
+        public async Task ListDirectoryDetails_RealNetwork_CheckDate()
+        {
+            var client = new FtpClient(realFtpUri, credentials);
+            var list = await client.ListDirectoryDetailsAsync(null!, CancellationToken.None);
+            Assert.NotEmpty(list);
+        }
+        [Fact]
+        public async Task MakeDirectoryTestTest_RealNetwork_CheckDate()
+        {
+            var client = new FtpClient(realFtpUri, credentials);
+            await client.MakeDirectoryAsync("test/test/unitest/");
+            client.ListDirectory("test/test/unitest/");
+        }
+    }
+}
---
+++ ValheimFTPSync.Test/FtpServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using ValheimFTPSync.Extensions;
+using ValheimFTPSync.Models;
+using ValheimFTPSync.Services;
+using ValheimFTPSync.Services.Interfaces;
+
+namespace ValheimFTPSync.Test
+{
+    public class FtpServiceTest
+    {
+
+        Uri _realFtpUri = new Uri("ftp://192.168.1.1:21/sda1/Valheim/");
+        NetworkCredential _credentials = new NetworkCredential();
+        IAppLogger _appLogger;
+        public FtpServiceTest()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var mock = new Mock<IAppLogger>();
+            mock.Setup(x => x.Info(It.IsAny<string>())).Callback<string>(str => Console.WriteLine(str));
+            _appLogger = mock.Object;
+        }
+
+        [Fact]
+        public async Task TryConnect_RealNetwork_ShouldReturnTrue()
+        {
+            FtpClientFactory ftpClientFactory = new FtpClientFactory(_appLogger);
+            var client = ftpClientFactory.CreateFtpClient(_realFtpUri.OriginalString, _credentials);
+            Assert.NotNull(client);
+            FtpService ftpService = new FtpService(client, _appLogger, new LocalFileService());
+            await ftpService.TryConnectAsync();
+        }
+
+        [Fact/*(Skip = "Real File")*/]
+        public async Task DownloadRealFiles_RealNetwork_ShouldReturnTrue()
+        {
+            FtpClientFactory ftpClientFactory = new FtpClientFactory(_appLogger);
+            var client = ftpClientFactory.CreateFtpClient(_realFtpUri.OriginalString, _credentials);
+            Assert.NotNull(client);
+            IFtpService service = new FtpService(client, _appLogger, new LocalFileService());
+
+            var dirAsync = await service.GetDirectoryInfoAsync("", CancellationToken.None);
+            var dir = service.GetDirectoryInfo("", CancellationToken.None);
+            Assert.Equivalent(dirAsync, dir);
+            Progress<OperationProgress> progress = new Progress<OperationProgress>(x => Console.WriteLine("[{0}] {1}: {2}/{3}:{4}% bytes. Files:{5}/{6}:{7}%", x.Operation, x.CurrentFile, x.TransferProgress.TransferredBytes, x.TransferProgress.TotalBytes, x.TransferProgress.Percent, x.CompletedFiles, x.TotalFiles, x.OverallProgress));
+            var localPath = "C:\\temp";
+            await service.DownloadFilesAsync(dir.GetFilesInDirectoryRecursive().ToArray(), localPath, progress);
+            Assert.NotEmpty(dir.Files);
+            Assert.All(dir.GetFilesInDirectoryRecursive(), x => File.Exists(Path.Combine(localPath, x.RelativePath)));
+        }
+
+        [Fact]
+        public async Task UploadRealFiles_RealNetwork_ShouldReturnTrue()
+        {
+            FtpClientFactory ftpClientFactory = new FtpClientFactory(_appLogger);
+            var client = ftpClientFactory.CreateFtpClient(_realFtpUri.OriginalString, _credentials);
+            Assert.NotNull(client);
+            IFtpService service = new FtpService(client, _appLogger, new LocalFileService());
+            var localPath = "C:\\temp\\";
+            var localPathFile = "C:\\temp\\tratra\\fwfkwfw\\аыфафафц\\фафцафф\\mysupperFile.txt";
+            var files = new[] { new FtpFileModel(Path.GetFileName(localPathFile), Path.GetRelativePath(localPath, localPathFile), DateTime.MinValue, 0) };
+            Progress<OperationProgress> progress = new Progress<OperationProgress>(x => Console.WriteLine("[{0}] {1}: {2}/{3}:{4}% bytes. Files:{5}/{6}:{7}%", x.Operation, x.CurrentFile, x.TransferProgress.TransferredBytes, x.TransferProgress.TotalBytes, x.TransferProgress.Percent, x.CompletedFiles, x.TotalFiles, x.OverallProgress));
+            await service.UploadFilesAsync(files, localPath, progress);
+        }
+    }
+}
